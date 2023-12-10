97d0f90c71e57c887cc4e57ae705119c78469f81
agmui <anthonynmui@gmail.com>
2023-12-09 20:00:33 -0500

    trying to build picotool

--- windows_install.ps1
+++ windows_install.ps1
@@ -41,7 +41,17 @@ if(-not($chocoexists)){
 refreshenv # to update enviromen vars
 
 # mingw contains gcc and python3.9
-choco install -y cmake --installargs 'ADD_CMAKE_TO_PATH=System' git vscode gcc-arm-embedded mingw ninja
+choco install -y cmake --installargs 'ADD_CMAKE_TO_PATH=System' git vscode gcc-arm-embedded mingw ninja# libusbdotnet
+
+# git clone https://github.com/raspberrypi/picotool.git
+
+# cd picotool-master
+# mkdir build
+# cd build
+# cmake -G "NMake Makefiles" ..
+# nmake
+
+# cd ../../
 
 refreshenv # to update enviromen vars
 

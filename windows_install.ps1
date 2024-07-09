df4465b350b469468c22a8f79017ea26e1ad2828
agmui <65274668+agmui@users.noreply.github.com>
2024-07-09 14:37:49 -0400

    added a change to home dir

--- windows_install.ps1
+++ windows_install.ps1
@@ -1,3 +1,5 @@
+cd "$env:USERPROFILE"
+
 if(!([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
  Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList "-File `"$($MyInvocation.MyCommand.Path)`"  `"$($MyInvocation.MyCommand.UnboundArguments)`""
  Exit
